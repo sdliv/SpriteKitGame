e8d92debb93da615668ea124a34022aea475f286
Sean Livingston <sean.d.livingston@gmail.com>
2017-05-03 14:10:51 -0400

    Adds physics categories

--- SpriteKitSimpleGame/GameScene.swift
+++ SpriteKitSimpleGame/GameScene.swift
@@ -9,6 +9,15 @@
 import SpriteKit
 import GameplayKit
 
+struct PhysicsCategory {
+    static let None         : UInt32 = 0
+    static let All          : UInt32 = UInt32.max
+    static let Monster      : UInt32 = 0b1
+    static let Projectile   : UInt32 = 0b10
+}
+
+
+
 func + (left: CGPoint, right: CGPoint) -> CGPoint {
     return CGPoint(x: left.x + right.x, y: left.y + right.y)
 }
